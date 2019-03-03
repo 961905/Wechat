dd36b5966273070c5269009eb1bbd1c25bd0ae0e
961905 <1357926740@qq.com>
2019-03-03 22:25:21 +0800

    .

---
+++ diary/Exercise code/三角形面积.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+main(){
+	double a,b,c,s,sum;
+	scanf("%f" "%f" "%f",a,b,c);
+	if(a+b>c&&a+c>b&&b+c>a){
+		s=(a+b+c)/2;
+		sum=s*(s-a)*(s-b)*(s-c);
+	    sum=sqrt(sum);
+	    printf("%.2f",sum);
+	}else{
+		printf("False");
+	}
+}
